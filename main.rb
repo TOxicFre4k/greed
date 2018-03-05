88a122ceb502eab9ed2286a8c8d3d84478bc0e2a
Jithin Mathew <mjithin4u@gmail.com>
2018-03-05 13:43:15 +0530

    Bug fixing

--- main.rb
+++ main.rb
@@ -16,22 +16,19 @@ class MainClass
     def roller
         logic = ThrowLogic.new
         loop do
-            loop do
-                (1..@number).each do |i|
-                    @player_score, @scoring_dies, @non_scoring_dies, @total_score = logic.throw(i)
-                    puts "\nPlayer score is: #{@player_score}"
-                end
-                print "\nDo you wish to continue to next round? : "
-                @continue = STDIN.gets.chomp.to_s
-                if @continue == 'Y' or @continue == 'y'
-                    next
-                end
-                break if @continue == 'n' or @continue == 'n'
-            end
             (1..@number).each do |i|
-                puts "\nTotal Score of - Player #{i}:  #{@total_score[i-1]}"
+                @player_score, @scoring_dies, @non_scoring_dies, @total_score = logic.throw(i)
+                puts "\nPlayer score is: #{@player_score}"
             end
-            break if @total_score[i-player] >= 3000
+            print "\nDo you wish to continue to next round? : "
+            @continue = STDIN.gets.chomp.to_s
+            if @continue == 'Y' || @continue == 'y'
+                next
+            end
+            break if @continue == 'n' || @continue == 'n' || @total_score[@number - (@number + 1 )] < 3000
+        end
+        (1..@number).each do |i|
+            puts "\nTotal Score of - Player #{i}:  #{@total_score[i-1]}"
         end
     end
 end
